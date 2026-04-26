5347bd11c446e1ebcdab2f0ae9b22095cb751af4
Candyfair <candice.bastien@gmail.com>
2026-04-26 20:05:17 +0200

    Add hook to fetch data

---
+++ src/hooks/useBatteries.js
@@ -0,0 +1,31 @@
+// Imports
+import { useEffect, useState } from "react";
+import { API_BASE_URL } from "@/lib/constants";
+
+// Export function useBatteries
+export function useBatteries() {
+  const [batteries, setBatteries] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  
+  useEffect(() => {
+    async function fetchBatteries() {
+      try {
+        const response = await fetch(`${API_BASE_URL}/batteries`)
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP : ${response.status}`)
+        }
+        const data = await response.json()
+        setBatteries(data)
+      } catch (err) {
+        setError(err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    
+    fetchBatteries()
+  }, [])
+
+  return {batteries, loading, error}    
+}
